a7c1a978cf3365d03f26ca001dd39e7b216e73e3
Shreyash Zala <shreyash.zala@tatvasoft.com>
2026-04-06 17:21:22 +0530

    Test Railway Setup

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,15 +1,25 @@
-import AWS from "aws-sdk";
+// import AWS from "aws-sdk";
 import mysql from "mysql2/promise";
 
-const ssm = new AWS.SSM({ region: "ap-south-1" });
+// const ssm = new AWS.SSM({ region: "ap-south-1" });
 
 async function getDBParams() {
-  const names = ["/myapp/DB_HOST","/myapp/DB_USER","/myapp/DB_PASS","/myapp/DB_NAME"];
-  const response = await ssm.getParameters({ Names: names, WithDecryption: true }).promise();
-  const params: Record<string, string> = {};
-  response.Parameters?.forEach(p => {
-    params[p.Name!.split("/").pop()!] = p.Value!;
-  });
+  // AWS Setup
+
+  // const names = ["/myapp/DB_HOST","/myapp/DB_USER","/myapp/DB_PASS","/myapp/DB_NAME"];
+  // const response = await ssm.getParameters({ Names: names, WithDecryption: true }).promise();
+  // const params: Record<string, string> = {};
+  // response.Parameters?.forEach(p => {
+  //   params[p.Name!.split("/").pop()!] = p.Value!;
+  // });
+
+  // Railway Setup
+  const params = {
+    DB_HOST : process.env.DB_HOST,
+    DB_USER : process.env.DB_USER,
+    DB_PASS : process.env.DB_PASS,
+    DB_NAME : process.env.DB_NAME,
+  }
   return params;
 }
 
