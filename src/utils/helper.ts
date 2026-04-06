f5d9e9ada9d915b8fca3a1d8ee07415a7728fe4c
Shreyash Zala <shreyash.zala@tatvasoft.com>
2026-04-06 18:25:21 +0530

    Fixed Login for Railway

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -13,13 +13,19 @@ export async function comparePassword(password: string, hash: string) {
 
 
 export async function getJWTSecret(): Promise<string> {
-  const ssm = new AWS.SSM({ region: "ap-south-1" });
-  const param = await ssm
-    .getParameter({
-      Name: "/myapp/JWT_SECRET", // replace with your parameter name
-      WithDecryption: true,      // decrypt if it's a SecureString
-    })
-    .promise();
+  // AWS Setup
+  
+  // const ssm = new AWS.SSM({ region: "ap-south-1" });
+  // const param = await ssm
+  //   .getParameter({
+  //     Name: "/myapp/JWT_SECRET", // replace with your parameter name
+  //     WithDecryption: true,      // decrypt if it's a SecureString
+  //   })
+  //   .promise();
+
+  // Railway Setup
+
+  const param = { Parameter : { Value: process.env.JWT_SECRET }}
 
   if (!param.Parameter?.Value) {
     throw new Error("JWT secret not found in SSM");
